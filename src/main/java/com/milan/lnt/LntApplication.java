a5f6c5f3ac1e7d0f29eac98d43adeed3e2e036d4
milaneuh <milanrgmnt@gmail.com>
2024-10-29 15:45:47 +0100

    Init spring project

---
+++ src/main/java/com/milan/lnt/LntApplication.java
@@ -0,0 +1,13 @@
+package com.milan.lnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LntApplication.class, args);
+	}
+
+}
