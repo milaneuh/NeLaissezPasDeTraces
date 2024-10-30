6f311cd916e92115e0b3810c12f1c560e5ae11c8
milaneuh <milanrgmnt@gmail.com>
2024-10-30 19:26:20 +0100

    Simplified quiz page structure

--- src/main/java/com/milan/lnt/Question.java
+++ src/main/java/com/milan/lnt/Question.java
@@ -1,6 +1,7 @@
 package com.milan.lnt;
 
 import java.util.Collection;
+import java.util.List;
 
-public record Question(int index, String question, Collection<Choice> choices, String answer) {
+public record Question(int index, String question, List<Choice> choices, String answer) {
 }
