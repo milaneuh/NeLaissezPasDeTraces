4d2c404997e0daae3b119363db115a7f422656d4
milaneuh <milanrgmnt@gmail.com>
2024-11-04 16:21:24 +0100

    Docker compose

--- src/test/java/com/milan/lnt/LntApplicationTests.java
+++ src/test/java/com/milan/lnt/LntApplicationTests.java
@@ -2,12 +2,11 @@ package com.milan.lnt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = { "spring.config.location=classpath:application.properties" })
 class LntApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
